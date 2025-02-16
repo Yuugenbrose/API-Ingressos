c3d90998dd265d960baf1c79ed80e158a8bf6fbe
Niran <149912977+Yuugenbrose@users.noreply.github.com>
2025-02-16 04:02:40 -0300

    Delete Projeto2 directory

--- Projeto2/middlewares/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-function isAdmin(req, res, next) {
-    if (req.user && req.user.paper === 'admin') {
-      return next();
-    }
-    return res.status(403).json({ erro: 'Permissão de administrador necessária' });
-  }
-  
-  module.exports = { isAdmin };
-  
